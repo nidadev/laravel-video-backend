e5d6a3020a6842149cc4f4267402387b04d7b911
nida <nidhfz85@gmail.com>
2025-12-02 08:51:53 +0500

    upd

---
+++ resources/views/emails/send_otp.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your OTP Code</title>
+</head>
+<body>
+    <p>Hello,</p>
+    <p>Your verification code is: <strong>{{ $otp }}</strong></p>
+    <p>This code will expire in 15 minutes.</p>
+    <p>Thank you!</p>
+</body>
+</html>
