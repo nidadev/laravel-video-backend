f9d42372914247a11f3adec8fe90b22ae448187d
nida <nidhfz85@gmail.com>
2026-03-09 10:35:39 +0500

    upd

--- app/Http/Controllers/Api/UserApiController.php
+++ app/Http/Controllers/Api/UserApiController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
+use App\Models\Subscription;
 
 class UserApiController extends Controller
 {
